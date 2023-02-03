5d6d2ac4d81eff971d72427b184b3dc56452151c
mojyack <mojyack@gmail.com>
2023-02-03 15:38:09 +0900

    improve debug output

--- src/debug.hpp
+++ src/debug.hpp
@@ -73,28 +73,6 @@ class Framebuffer {
 
 inline auto fb = (Framebuffer*)(nullptr);
 
-template <class T>
-concept Number = std::integral<T> || std::is_pointer_v<T>;
-
-template <Number T>
-auto itos(const T value) -> std::array<char, sizeof(T) * 2 + 1> {
-    auto r = std::array<char, sizeof(T) * 2 + 1>();
-
-    for(auto i = 0; i < sizeof(T) * 2; i += 1) {
-        auto c = (value >> 4 * (sizeof(T) * 2 - i - 1)) & 0x0Fu;
-        if(c >= 10) {
-            c += 'a' - 10;
-        } else {
-            c += '0';
-        }
-        r[i] = c;
-    }
-
-    r.back() = '\0';
-
-    return r;
-}
-
 inline auto draw_ascii(const Point point, const char c) -> void {
     if(fb == nullptr) {
         return;
@@ -119,8 +97,24 @@ inline auto draw_string(const Point point, const std::string_view str) -> void {
 
 inline auto cursor = std::array<int, 2>{0, 0};
 
+struct Number {
+    size_t   data;
+    uint32_t base;
+    uint32_t fill;
+
+    Number(const auto data, const uint32_t base = 16, const uint32_t fill = 16) : data(static_cast<size_t>(data)),
+                                                                                  base(base),
+                                                                                  fill(fill) {}
+};
+
 template <class T>
-concept Printable = Number<T> || std::is_convertible_v<T, std::string_view>;
+concept NumberLike = std::is_constructible_v<Number, T>;
+
+inline auto print(const char c) -> void {
+    const auto font_size = get_font_size();
+    draw_ascii(Point(cursor[0], cursor[1]), c);
+    cursor[0] += font_size[0];
+}
 
 inline auto print(const std::string_view str) -> void {
     const auto font_size = get_font_size();
@@ -128,42 +122,71 @@ inline auto print(const std::string_view str) -> void {
     cursor[0] += font_size[0] * str.size();
 }
 
-inline auto print(const char* const str) -> void {
+inline auto print(const char* str) -> void {
     print(std::string_view(str));
 }
 
-template <Number T>
-auto print(T value) -> void {
-    auto str = itos(value);
-    print(std::string_view(str.data(), str.size()));
+inline auto print(const Number number) -> void {
+    if(number.data == 0) {
+        const auto fill = number.fill == 0 ? 1 : number.fill;
+        for(auto i = 0; i < fill; i += 1) {
+            print('0');
+        }
+        return;
+    }
+
+    auto buf = std::array<char, sizeof(size_t) * 8>();
+    auto num = number.data;
+    auto i   = -1;
+    while(num != 0) {
+        i += 1;
+        const auto mod = num % number.base;
+        buf[i]         = mod >= 10 ? (mod - 10) + 'a' : mod + '0';
+        num /= number.base;
+    }
+    while((i + 1) < number.fill) {
+        i += 1;
+        buf[i] = '0';
+    }
+    while(i >= 0) {
+        print(buf[i]);
+        i -= 1;
+    }
 }
 
+template <NumberLike T>
+auto print(const T number) -> void {
+    print(Number(number));
+}
+
+template <class T>
+concept Printable = std::is_same_v<T, Number> ||
+                    std::is_same_v<T, char> ||
+                    std::is_same_v<T, std::string_view> ||
+                    std::is_same_v<T, const char*> ||
+                    NumberLike<T>;
+
 template <Printable Arg, Printable... Args>
-auto print(Arg arg, Args... args) -> void {
-    if constexpr(std::is_convertible_v<Arg, std::string_view>) {
-        print(std::string_view(arg));
-    } else {
-        print(arg);
-    }
-    if constexpr(sizeof...(args) >= 1) {
-        print(args...);
-    }
+auto print(const Arg arg, const Args... args) -> void {
+    print(arg);
+    print(args...);
 }
 
 template <Printable... Args>
-auto println(Args... args) -> void {
-    print(args...);
-
+auto println(const Args... args) -> void {
     constexpr auto line_width = 2;
 
     const auto size      = fb->get_size();
     const auto font_size = get_font_size();
+    fb->write_rect(Point(0, cursor[1]), Point(size[0], cursor[1] + font_size[1]), RGBColor(0x000000));
+    fb->write_rect(Point(0, cursor[1] + font_size[1]), Point(size[0], cursor[1] + font_size[1] + line_width), RGBColor(0xFFFFFF));
+
+    print(args...);
+
     cursor[1] += font_size[1];
     if(cursor[1] + line_width >= size[1]) {
         cursor[1] = 0;
     }
     cursor[0] = 0;
-    fb->write_rect(Point(0, cursor[1]), Point(size[0], cursor[1] + font_size[1]), RGBColor(0x000000));
-    fb->write_rect(Point(0, cursor[1] + font_size[1]), Point(size[0], cursor[1] + font_size[1] + line_width), RGBColor(0xFFFFFF));
 }
 } // namespace debug
--- src/libc-support.cpp
+++ src/libc-support.cpp
@@ -1,5 +1,5 @@
 #include "libc-support.hpp"
-//#include "print.hpp"
+#include "debug.hpp"
 
 extern "C" {
 void _exit(void) {
@@ -9,16 +9,27 @@ void _exit(void) {
 }
 
 caddr_t sbrk(const int incr) {
-   // constexpr auto heap_bytes = 64 * 512 * 4096;
-   // 
-   // const auto usage = 100.0 * (program_break_end - program_break - incr) / heap_bytes;
-   // printk("heap %d%%\n", int(usage));
+    // debug stuff
+    static auto initial_break = caddr_t(0);
+    static auto size_bytes    = size_t(0);
+    if(initial_break == 0) {
+        initial_break = program_break;
+        size_bytes    = program_break_end - program_break;
+    }
+    {
+        using namespace debug;
+        const auto remain  = program_break_end - program_break - incr;
+        const auto percent = 1. * remain / size_bytes * 100;
+        println("sbrk: ", Number(remain, 16, 0), "/", Number(size_bytes, 16, 0), " (", Number(percent, 10, 0), "%)");
+    }
+    // ~debug stuff
+
     if(program_break == 0 || program_break + incr >= program_break_end) {
         errno = ENOMEM;
         return (caddr_t)-1;
     }
 
-    caddr_t prev_break = program_break;
+    const auto prev_break = program_break;
     program_break += incr;
     return prev_break;
 }
